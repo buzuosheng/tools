93a6a72d00207e6cdb709152cbbb4b16e10c65b2
wangqi <1015350043@qq.com>
2020-02-02 18:49:07 +0800

    base64完成

--- src/components/Base64/index.tsx
+++ src/components/Base64/index.tsx
@@ -1,14 +1,42 @@
 import React from 'react'
 import { useState } from 'react'
 import './index.css'
-const base64 = require('Base64')
+import base64 from 'base64-js'
+
+const stringToUint8Array = (str:string) => {
+  const arr = Array.from(str, char => char.charCodeAt(0))
+
+  const tmpUint8Array = new Uint8Array(arr);
+  return tmpUint8Array
+
+}
+
+function Uint8ArrayToString(fileData:Uint8Array){
+  let dataString = "";
+  for (let i = 0; i < fileData.length; i++) {
+    dataString += String.fromCharCode(fileData[i]);
+  }
+ 
+  return dataString
+}
+
+// const sToUInt8 = (str:string) => {
+//   const arr:number[] = [];
+//   const newstr = str.split('')
+//   newstr.map(
+//     (item, index) => {
+//       arr.push(item.charCodeAt(index))
+//     }
+//   )
+// }
 
 const Base64: React.FC = () => {
 
+
   const [code, setCode] = useState('')
   const [result, setResult] = useState('')
-  const btoa = base64.btoa;
-  const atob = base64.atob;
+
+  const res = Uint8ArrayToString(base64.toByteArray(Uint8ArrayToString(stringToUint8Array(result))))
 
   return (
     <div className='base64-container'>
@@ -21,14 +49,19 @@ const Base64: React.FC = () => {
           onChange={(e) => { setCode(e.target.value) }}
         />
       </div>
-      <button className='base64-btn' onClick={() => setResult(atob(code))}>
+      <button className='base64-btn' onClick={() => setResult(base64.fromByteArray(stringToUint8Array(code)))}>
         编码
       </button>
-      <button className='base64-btn' onClick={() => setCode(btoa(result))}>
+      <button className='base64-btn' onClick={() => setCode(res)}>
         解码
       </button>
       <h3>加密后的结果为：</h3>
-      <div className='base64-result'>{result}</div>
+      <div>
+        <textarea 
+          value={result}
+          onChange={(e) => { setResult(e.target.value)}}
+         />
+      </div>
     </div>
   )
 }
