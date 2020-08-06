2324e309289b8b2a5aa1a8a62c20a91bc9edd741
wangqi <1015350043@qq.com>
2020-08-06 21:17:53 +0800

    actions

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Test from './App.test'
 import * as serviceWorker from './serviceWorker';
 
 
